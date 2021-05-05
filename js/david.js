ec0f1994bacd94d3c3d64ef5c82c53f71dceef8c
David <David.sanchezm@campusfp.es>
2021-05-05 14:46:37 +0200

    Carpeta recursos añadida

--- js/david.js
+++ js/david.js
@@ -7,16 +7,18 @@ function procesarRespuesta () {
 	if (ajax.readyState == 4) {
 		if (ajax.status == 200) {
 			capa.innerText ="Agenda cargada";
+			console.log('Agenda cargada');
 			
 			
 		}
 		else {
 			capa.innerText = ajax.status+"Error AJAX, no se puede obtener la agenda";
+			console.log('Error AJAX, no se puede obtener la agenda');
 		}
 	}
 }
 
 function loadlista() {
-	ajax.open("GET", "biblioteca.json");
+	ajax.open("GET", "recursos_david/jsonDavid/biblioteca.json");
     ajax.send();
 }
